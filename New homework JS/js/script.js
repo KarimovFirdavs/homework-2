cc4f972d2992cbd349e9cda37df62d82ac376968
KarimovFirdavsFE[14] <115251742+KarimovFirdavs@users.noreply.github.com>
2022-10-11 17:56:25 +0500

    Add files via upload

---
+++ New homework JS/js/script.js
@@ -0,0 +1,11 @@
+let name = prompt(' Card name ');
+console.log(' Card name entered ' + name);
+let age = prompt(' Enter card number ');
+console.log(age);
+let info = (' Loading...Everything went well ')
+
+if (age == 8888444484848888) {
+    console.log(' Card number entered ' + info);
+} else {
+    console.log(' EROR ')
+}
